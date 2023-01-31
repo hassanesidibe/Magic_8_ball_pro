b27925064f5ae95988de55496cdbc24d5dfc36f9
hassanesidibe <hassanesidibe5@gmail.com>
2023-01-31 13:46:25 -0500

    Initial Commit

---
+++ Magic_8_ball_pro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic_8_ball_pro
+//
+//  Created by Hassane Sidibe on 1/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
