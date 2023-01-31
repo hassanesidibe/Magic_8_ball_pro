747dd03ecdb2ecd7b715b8daf92d2355ca4ef8af
hassanesidibe <hassanesidibe5@gmail.com>
2023-01-31 14:33:05 -0500

    I updated the app icon

--- Magic_8_ball_pro/ViewController.swift
+++ Magic_8_ball_pro/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    private let ballImages = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    @IBOutlet weak var ballImageView: UIImageView!
+    
+    @IBAction func askButtonPressed(_ sender: Any) {
+        let chosenImage = ballImages.randomElement()!
+        ballImageView.image = UIImage(named: chosenImage)
     }
-
+    
 
 }
 
